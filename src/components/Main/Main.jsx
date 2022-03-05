6f831a9ef77613c7f317df4dd9bb6945aa1ff6bc
Long <longish222@gmail.com>
2022-03-04 20:35:54 -0800

    aligned text relative to picture and has state

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,7 +4,9 @@ import memesData from "../../memesData";
 
 const Main = () => {
 
-  const [url, setUrl] = useState('https://i.imgflip.com/4acd7j.png')
+  const [url, setUrl] = useState('https://i.imgflip.com/4acd7j.png');
+  const [topText, setTopText] = useState('TOP TEXT');
+  const [bottomText, setBottomText] = useState('BOTTOM TEXT');
 
   const getImage = () => {
     // create a randomizer
@@ -19,6 +21,13 @@ const Main = () => {
     // console.log(url)
   };
 
+  const handleChangeTopText = (event) => {
+    setTopText(event.target.value)
+  }
+  const handleChangeBottomText = (event) => {
+    setBottomText(event.target.value)
+  }
+
   return (
     <>
       {/* text input for top and bottom */}
@@ -28,11 +37,13 @@ const Main = () => {
             className="main-input"
             type="text"
             placeholder="Top text here..."
+            onChange={handleChangeTopText}
           ></input>
           <input
             className="main-input"
             type="text"
             placeholder="Bottom text here..."
+            onChange={handleChangeBottomText}
           ></input>
         </div>
       </form>
@@ -46,12 +57,14 @@ const Main = () => {
 
       {/* PICTURE GENERATOR */}
       <div className="main-image-area">
+        <h3 className="main-image-top-text">{topText}</h3>
         <img
           className="main-image"
           src={
             url
           }
         />
+        <h3 className="main-image-bottom-text">{bottomText}</h3>
       </div>
     </>
   );
